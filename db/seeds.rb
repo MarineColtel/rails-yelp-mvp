1dfa7bcbe62cbdcf671dd4ce9b34a351c20bf801
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-14 22:20:19 +0100

    Yelp MVP exercise finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating restaurants...'
+tour_d_argent = Restaurant.new(name: "La Tour d'Argent", address: "1 boulevard Lila", category: "chinese" )
+tour_d_argent.save!
+
+chez_gladines = Restaurant.new(name: "Chez Gladines", address: "104 avenue Angers", category: "italian")
+chez_gladines.save!
+
+tonton_foch = Restaurant.new(name: "Tonton Foch", address: "10 boulevard Foch", category: "french")
+tonton_foch.save!
+
+bistrot = Restaurant.new(name: "Bistrot", address: "15 rue Lenepveu", category: "belgian")
+bistrot.save!
+
+sushi_restaurant = Restaurant.new(name: "Sushi restaurant", address: "26 route de Leon", category: "japanese")
+sushi_restaurant.save!
+puts 'Finished!'
